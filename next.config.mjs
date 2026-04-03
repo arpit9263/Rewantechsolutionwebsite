bd41a42eca46edbc987dc7796b62741257ff30ff
Arpit Yadav <ay835390@gmail.com>
2026-04-03 18:27:48 +0530

    changes config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   output: 'export',
-
   images: {
     unoptimized: true,
   },
+
+  // 🔥 ADD THIS
+  trailingSlash: true,
 };
 
 export default nextConfig;
